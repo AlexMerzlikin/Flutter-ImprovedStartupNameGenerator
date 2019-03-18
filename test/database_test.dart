5bdd9d3693909ec40b5545658ea61ebacc1a14f2
Alex M <alexgotfrag@gmail.com>
2019-03-19 00:30:38 +0300

    Database tests

---
+++ test/database_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_app/database/file_database.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("Database", () {
+    test("File should be saved", () {
+      final database = new FileDatabase();
+      final file = database.save(["1"]);
+      expect(file != null, true);
+    });
+
+    test("Data should be loaded", () {
+      final database = new FileDatabase();
+      database.save(["1"]);
+      final data = database.load();
+      data.then((list) => expect(list.contains("1"), true));
+    });
+  });
+}
